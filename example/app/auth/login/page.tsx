9c35d06867463240ff69756074912c3cfaf96374
Mohd. Nisab <nisabmohd@gmail.com>
2024-07-02 23:01:04 +0530

    update example with new version

--- example/app/auth/login/page.tsx
+++ example/app/auth/login/page.tsx
@@ -1,12 +1,19 @@
-import { UserCredentials, login } from "@/lib/actions";
+import { EXPIRY } from "@/lib";
+import { createSession, User } from "@ease-session/auth";
 import { redirect } from "next/navigation";
 
 export default function LoginPage() {
   async function handleLogin(formdata: FormData) {
     "use server";
-    const userobj = Object.fromEntries(formdata);
-    await login(userobj as UserCredentials);
-    redirect("/");
+    const userobj = Object.fromEntries(formdata) as User;
+    const { password, email } = userobj;
+    if (password == "john123" && email == "johndoe@gmail.com") {
+      await createSession(
+        { email, username: "john" },
+        { expiresAfter: EXPIRY }
+      );
+      redirect("/");
+    }
   }
   return (
     <form
--- example/middleware.ts
+++ example/middleware.ts
@@ -1,6 +1,6 @@
 import { updateSession } from "@ease-session/auth";
 import { NextRequest } from "next/server";
-import { EXPIRY } from "./lib/util";
+import { EXPIRY } from "./lib";
 
 export async function middleware(req: NextRequest) {
   return await updateSession(req, { expiresAfter: EXPIRY });
